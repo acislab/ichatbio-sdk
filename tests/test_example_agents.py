21e87e693b618d760f1382c97fbf236765ec40ba
Michael <mielliott@ufl.edu>
2025-06-11 12:57:31 -0400

    Add iDigBio example agent

--- tests/test_example_agents.py
+++ tests/test_example_agents.py
@@ -1,6 +1,7 @@
 import pytest
 
 from examples.cataas.agent import CataasAgent, GetCatImageParameters
+from examples.idigbio.agent import IDigBioAgent
 from ichatbio.types import ArtifactMessage, ProcessMessage
 
 
@@ -25,3 +26,30 @@ async def test_cataas():
     assert artifact.mimetype == "image/png"
     assert artifact.content
     assert artifact.metadata == {"api_query_url": "https://cataas.com/cat/sphynx"}
+
+
+@pytest.mark.asyncio
+async def test_idigbio():
+    agent = IDigBioAgent()
+    response = agent.run("Retrieve records of Rattus rattus in Alaska", "find_occurrence_records", None)
+    messages = [m async for m in response]
+
+    process_messages: list[ProcessMessage] = [p for p in messages if type(p) is ProcessMessage]
+    assert len(process_messages) == 5
+
+    search_parameters = next(p.data for p in process_messages if p.description == "Generated search parameters")
+    assert search_parameters == {
+        "limit": 100,
+        "rq": {
+            "scientificname": "Rattus rattus",
+            "stateprovince": "Alaska"
+        }
+    }
+
+    artifacts = [p for p in messages if type(p) is ArtifactMessage]
+    assert len(artifacts) == 1
+
+    artifact: ArtifactMessage = artifacts[0]
+    assert artifact.mimetype == "application/json"
+    assert artifact.uris
+    assert artifact.metadata["data_source"] == "iDigBio"
