55fc2e5208549e987c556c9e119269dfed7a60c5
Michael <mielliott@ufl.edu>
2025-06-16 18:10:46 -0400

    More comments

--- examples/idigbio/agent.py
+++ examples/idigbio/agent.py
@@ -15,12 +15,14 @@ class IDigBioAgent(IChatBioAgent):
             description="Searches for information in the iDigBio portal (https://idigbio.org).",
             icon=None,
             entrypoints=[
+                # Because this agent is planned to have many entrypoints, we define them in their own files
                 find_occurrence_records.entrypoint
             ]
         )
 
     @override
     async def run(self, request: str, entrypoint: str, params: Optional[BaseModel]) -> AsyncGenerator[None, Message]:
+        # Route requests to their selected entrypoints
         coro: AsyncIterator[Message]
         match entrypoint:
             case find_occurrence_records.entrypoint.id:
@@ -28,5 +30,6 @@ class IDigBioAgent(IChatBioAgent):
             case _:
                 raise ValueError()
 
+        # Execute the entrypoint code (an asyncio coroutine)
         async for message in coro:
             yield message
